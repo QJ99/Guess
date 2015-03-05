b073e5a6907208ae74aa64e15c99b32fba2b7428
JoyIos <646896350@qq.com>
2015-03-05 15:18:54 +0800

    xxx

---
+++ Guess/BassClass/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  Guess
+//
+//  Created by QJ on 15/3/5.
+//  Copyright (c) 2015年 QJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
---
+++ Guess/Controller/MainController/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Guess
+//
+//  Created by QJ on 15/3/5.
+//  Copyright (c) 2015年 QJ. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface MainViewController : BaseViewController
+
+@end
