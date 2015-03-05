ead10f487c603325947cbf89ec6d3e981fa74093
JoyIos <646896350@qq.com>
2015-03-05 17:33:05 +0800

    build

---
+++ Guess/BassClass/MyNavViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyNavViewController.h
+//  Guess
+//
+//  Created by QJ on 15/3/5.
+//  Copyright (c) 2015年 QJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyNavViewController : UINavigationController
+
+@end
---
+++ Guess/BassClass/MyNavViewController.m
@@ -0,0 +1,37 @@
+//
+//  MyNavViewController.m
+//  Guess
+//
+//  Created by QJ on 15/3/5.
+//  Copyright (c) 2015年 QJ. All rights reserved.
+//
+
+#import "MyNavViewController.h"
+
+@interface MyNavViewController ()
+
+@end
+
+@implementation MyNavViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
